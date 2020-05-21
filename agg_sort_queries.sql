eb1226f23991ebdc10f9ac35e8e37daa555247d0
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-21 17:06:02 -0400

    Working through Aggregation and Sorting Queries, Exercise 1

---
+++ agg_sort_queries.sql
@@ -0,0 +1,16 @@
+-- List restaurants by the closest distance.
+SELECT restaurant_name FROM restaurant ORDER BY distance ASC;
+
+-- List the top 2 restaurants by distance.
+
+-- List the top 2 restaurants by stars.
+
+-- List the top 2 restaurants by stars where the distance is less than 2 miles.
+
+-- Count the number of restaurants in the db.
+
+-- Count the number of restaurants by category.
+
+-- Get the average stars per restaurant by category.
+
+-- Get the max stars of a restaurant by category.
