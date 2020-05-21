ee5e727e1c2b33a70dab84757847064273804db5
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-21 17:36:00 -0400

    Aggregation and Sorting Queries for PostgreSQL Exercise 1 complete

--- agg_sort_queries.sql
+++ agg_sort_queries.sql
@@ -2,15 +2,25 @@
 SELECT restaurant_name FROM restaurant ORDER BY distance ASC;
 
 -- List the top 2 restaurants by distance.
+SELECT restaurant_name FROM restaurant ORDER BY distance ASC LIMIT 2;
 
 -- List the top 2 restaurants by stars.
+SELECT restaurant_name FROM restaurant ORDER BY stars DESC LIMIT 2;
 
 -- List the top 2 restaurants by stars where the distance is less than 2 miles.
+SELECT restaurant_name FROM restaurant WHERE distance <= 2 ORDER BY stars DESC LIMIT 2;
 
 -- Count the number of restaurants in the db.
+SELECT COUNT(*) FROM restaurant;
 
 -- Count the number of restaurants by category.
+SELECT COUNT(*) FROM restaurant WHERE category = 'Mexican';
+SELECT COUNT(*) FROM restaurant WHERE category = 'Donuts';
 
 -- Get the average stars per restaurant by category.
+SELECT category, AVG(stars) FROM restaurant GROUP BY category;
+SELECT AVG(stars) FROM restaurant;
 
 -- Get the max stars of a restaurant by category.
+SELECT category, MAX(stars) FROM restaurant GROUP BY category;
+SELECT MAX(stars) FROM restaurant;
